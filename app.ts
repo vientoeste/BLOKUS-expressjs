278533dcfcf2bc46b59b1385e525701022a63a78
este <djjd2000@naver.com>
2023-01-03 23:50:21 +0900

    initialize to use nunjucks

--- app.ts
+++ app.ts
@@ -1,11 +1,24 @@
 import express, {
   Request, Response,
 } from 'express';
+import path from 'path';
+import nunjucks from 'nunjucks';
 
 const app = express();
+const dirname = __dirname.slice(0, -4);
+
+app.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+app.use(express.static(path.join(dirname, 'public')));
 
 app.route('/').get((req: Request, res: Response) => {
-  res.send('foo');
+  // res.sendFile(path.join(dirname, 'view', 'ex.html'));
+  res.render('main', { title: 'test' });
 });
 
 app.listen(3000);
