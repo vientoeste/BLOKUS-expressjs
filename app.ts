81de822305e0dda4509a146f462c3aba791e89c1
este <djjd2000@naver.com>
2023-03-10 16:05:20 +0900

    Feature: added express-session with redis

--- app.ts
+++ app.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import nunjucks from 'nunjucks';
 import { fileURLToPath } from 'url';
 import morgan from 'morgan';
+import RedisStore from 'connect-redis';
+import { createClient } from 'redis';
+import session from 'express-session';
 const dirname = fileURLToPath(new URL('.', import.meta.url));
 
 import mainRouter from './router/index.js';
@@ -18,7 +21,22 @@ nunjucks.configure('views', {
   watch: true,
 });
 
+const redisClient = createClient();
+
 app.use(express.static(path.join(dirname, 'public')));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: String(process.env.COOKIE_SECRET),
+  name: 'sessionId',
+  cookie: {
+    httpOnly: true,
+    secure: true,
+  },
+  store: new RedisStore({
+    client: redisClient,
+  }),
+}));
 app.use(morgan('dev'));
 app.use('/', mainRouter);
 app.use('/auth', authRouter);
