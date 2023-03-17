557c5f11d7d90578f7a87c01afdf6d036d08c602
este <djjd2000@naver.com>
2023-03-17 11:26:07 +0900

    Feature: designed routers and added annotation for test cases(for applying TDD)

--- app.test.ts
+++ app.test.ts
@@ -9,6 +9,92 @@ describe('GET /ex', () => {
   });
 });
 
+// RESTful api + proper WS configuration
+// [TODO] must add the test cases before development(for proper TDD)
+/**
+ * GET /auth
+ * main page
+ * provides sign in/up/out form
+ */
+
+/**
+ * POST /auth/signIn
+ * sign in request
+ */
+
+/**
+ * POST /auth/signOut
+ * sign out request
+ */
+
+/**
+ * POST /users
+ * sign up request
+ */
+
+/**
+ * GET /users
+ * display user info
+ */
+
+/**
+ * POST /games
+ * make a room for game
+ */
+
+/**
+ * GET /games
+ * display a list of rooms
+ */
+
+/**
+ * GET /games/{game_uuid}
+ * [optional] display room info(game settings)
+ */
+
+/**
+ * PUT /games/{game_uuid}
+ * [optional] update room info(game settings)
+ */
+
+/**
+ * DELETE /games/{game_uuid}
+ * delete game room, only for creator
+ */
+
+/**
+ * POST /games/{game_uuid}/join
+ * join the room
+ */
+
+/**
+ * WS {game_uuid} - start
+ * start the game(creator only)
+ * emit: creator
+ * listen: participants. start the game and set clients' variable about game info
+ */
+
+/**
+ * WS {game_uuid} - stop
+ * stop the game(creator only)
+ * emit: creator
+ * listen: participants. stop the game and count the score
+ */
+
+/**
+ * WS, {game_uuid} - surrender
+ * surrender the game
+ * emit: participant who wants to surrender
+ * listen: remaining participants must play the quit player
+ */
+
+/**
+ * WS {game_uuid} - putBlock
+ * put the block on the right turn of each participants
+ * emit: put the block
+ * listen: receive the block data and update one's board
+ */
+
 afterAll(async () => {
   server.close();
   // [TODO] needs mongodb disconnect here
