a4aedaa482fae4dd9ae8d10514f9bdd59af92dcc
este <djjd2000@naver.com>
2023-03-17 23:54:17 +0900

    F/C: added CustomError class and error handling middleware

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import nunjucks from 'nunjucks';
 import morgan from 'morgan';
@@ -9,6 +9,22 @@ import session from 'express-session';
 
 import mainRouter from './router/index.js';
 import authRouter from './router/auth.js';
+
+class CustomError extends Error {
+  constructor(message: string, status?: number) {
+    super(message);
+    Object.defineProperty(this, 'name', {
+      configurable: true,
+      writable: false,
+      value: 'Error',
+    });
+    if (status) {
+      this.status = status;
+    }
+  }
+
+  status?: number;
+}
 
 const app = express();
 
@@ -39,6 +55,19 @@ app.use(session({
 app.use(morgan('dev'));
 app.use('/', mainRouter);
 app.use('/auth', authRouter);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new CustomError(`Not existing router: ${req.method} ${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err: CustomError, req: Request, res: Response, next: NextFunction) => {
+  res.locals.message = err.message;
+  res.locals.error = err;
+  res.status(err.status ?? 500);
+  res.redirect(`${req.url}/?error=${err.message}`);
+});
 
 export const server = app.listen(3000);
 
